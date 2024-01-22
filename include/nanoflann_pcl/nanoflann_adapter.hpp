cae15675ca95bec72267b23425701cc18f387e15
Julian Gaal <julian.gaal@anavs.de>
2024-01-22 12:08:27 +0100

    comments, license [skip ci]

--- include/nanoflann_pcl/nanoflann_adapter.hpp
+++ include/nanoflann_pcl/nanoflann_adapter.hpp
@@ -1,3 +1,23 @@
+// Copyright (c) 2024 Julian Gaal
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in all
+// copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+// SOFTWARE.
+
 #pragma once
 
 #include <memory>
@@ -5,12 +25,17 @@
 #include <pcl/point_cloud.h>
 #include <pcl/point_types.h>
 
-static_assert(NANOFLANN_VERSION == 0x150 || NANOFLANN_VERSION == 0x151 || NANOFLANN_VERSION == 0x152 ||
-                  NANOFLANN_VERSION == 0x153,
+static_assert(NANOFLANN_VERSION == 0x150 ||     //
+                  NANOFLANN_VERSION == 0x151 || //
+                  NANOFLANN_VERSION == 0x152 || //
+                  NANOFLANN_VERSION == 0x153,   //
               "Unsupported nanoflann version. Supported are version 1.5.0-1.5.3");
 
 namespace nanoflann {
 
+/**
+ * @brief Struct mirroring the PCL FLANN interface, providing knn and radius search.
+ */
 template <typename PointT> class KdTreeFLANN {
   public:
     using PointCloud = typename pcl::PointCloud<PointT>;
@@ -22,34 +47,107 @@ template <typename PointT> class KdTreeFLANN {
     using IndicesPtr = std::shared_ptr<std::vector<int>>;
     using IndicesConstPtr = std::shared_ptr<const std::vector<int>>;
 
-    KdTreeFLANN(bool sorted = false);
+    KdTreeFLANN(bool sorted = true);
 
     ~KdTreeFLANN() = default;
 
+    /**
+     * @brief search for eps-approximate neighbours (default: 0)
+     * @param eps The epsilon value to set.
+     */
     inline void setEpsilon(float eps) { _params.eps = eps; }
 
+    /**
+     * @brief Sets whether the results should be sorted.
+     * @param sorted Boolean indicating whether to sort the results.
+     */
     inline void setSortedResults(bool sorted) { _params.sorted = sorted; }
 
+    /**
+     * @brief Sets the input point cloud for processing.
+     * @param cloud Constant pointer to the input point cloud.
+     * @param indices (Optional) constant pointer to indices for queries against the input cloud.
+     */
     void setInputCloud(const PointCloudConstPtr& cloud, const IndicesConstPtr& indices = IndicesConstPtr());
 
+    /**
+     * @brief Returns a constant pointer to the input point cloud.
+     * @return Constant pointer to the input point cloud.
+     */
     inline PointCloudConstPtr getInputCloud() const { return _adaptor.pcl; }
 
+    /**
+     * @brief Finds the k-nearest neighbors of a given point in the point cloud.
+     *
+     * This function identifies the k-nearest neighbors of the specified point
+     * in the point cloud. The indices and squared distances of the nearest points
+     * are returned in the provided vectors.
+     *
+     * @param point The query point for the nearest neighbors search.
+     * @param k The number of nearest neighbors to find.
+     * @param k_indices A vector to store the indices of the k-nearest neighbors.
+     * @param k_sqr_distances A vector to store the squared distances of the k-nearest neighbors.
+     *
+     * @return The number of nearest neighbors found (may be less than k if not enough
+     *         neighbors are available in the point cloud).
+     *
+     */
     int nearestKSearch(const PointT& point, int k, std::vector<int>& k_indices,
                        std::vector<float>& k_sqr_distances) const;
 
+    /**
+     * @brief Performs a radius search around a given point in the point cloud.
+     *
+     * This function finds all points within a specified radius from the given point
+     * in the point cloud. The indices and squared distances of the neighboring points
+     * are returned in the provided vectors.
+     *
+     * @param point The query point for the radius search.
+     * @param radius The radius within which points are considered neighbors, in meters.
+     * @param k_indices A vector to store the indices of neighboring points.
+     * @param k_sqr_distances A vector to store the squared distances of neighboring points.
+     *
+     * @return return point indices and squared distances found within the specified radius.
+     */
     int radiusSearch(const PointT& point, float radius, std::vector<int>& k_indices,
                      std::vector<float>& k_sqr_distances) const;
 
   protected:
     nanoflann::SearchParameters _params;
 
+    /**
+     * @brief Struct representing a PCL point cloud adaptor, providing knn and radius search.
+     * Matches this interface https://jlblancoc.github.io/nanoflann/structnanoflann_1_1KDTreeEigenMatrixAdaptor.html
+     */
     struct PointCloud_Adaptor {
+
         PointCloud_Adaptor() : pcl(), indices() {}
+
         ~PointCloud_Adaptor() = default;
+        /**
+         * @brief Gets the point count for the k-d tree.
+         * @return The number of points in the k-d tree.
+         */
         inline size_t kdtree_get_point_count() const;
+
+        /**
+         * @brief Gets the value of a point at a specified index and dimension.
+         * @param idx Index of the point.
+         * @param dim Dimension of the point. (in S03 0, 1, or 2)
+         * @return The value of the point at the specified index and dimension.
+         */
         inline float kdtree_get_pt(const size_t idx, int dim) const;
+
+        /**
+         * @brief Gets the bounding box for the k-d tree (template implementation).
+         * @tparam BBOX The bounding box type.
+         * @param[out] bbox The bounding box to be filled.
+         * @return Always returns false: Nanoflann assumes adaptor needs no special handling
+         */
         template <class BBOX> bool kdtree_get_bbox(BBOX&) const { return false; }
+
         PointCloudConstPtr pcl;
+
         IndicesConstPtr indices;
     };
 
@@ -91,7 +189,7 @@ template <typename PointT>
 inline int KdTreeFLANN<PointT>::radiusSearch(const PointT& point, float radius, std::vector<int>& k_indices,
                                              std::vector<float>& k_sqr_distances) const {
     static std::vector<nanoflann::ResultItem<int, float>> result;
-    result.reserve(128);
+    result.reserve(256);
 
     // From nanoflann README
     // > Important note: If L2 norms are used, notice that search radius and all passed and returned      distances are
